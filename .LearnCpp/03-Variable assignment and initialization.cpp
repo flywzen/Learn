fef5333aade41647a8aadc7506556cf463bae6b7
rafly <144429611+flywzen@users.noreply.github.com>
2025-03-20 18:56:23 +0700

    Update 03-Variable assignment and initialization.cpp
    Signed-off-by: rafly  <144429611+flywzen@users.noreply.github.com>

--- .LearnCpp/03-Variable assignment and initialization.cpp
+++ .LearnCpp/03-Variable assignment and initialization.cpp
@@ -1,9 +1,6 @@
 int main()
 {
-    // An integer can only hold non-fractional values.
-    // Initializing an int with fractional value 4.5 requires the compiler to convert 4.5 to a value an int can hold.
-    // Such a conversion is a narrowing conversion, since the fractional part of the value will be lost.
-
+   
     int w1 { 4.5 }; // compile error: list-init does not allow narrowing conversion
 
     int w2 = 4.5;   // compiles: w2 copy-initialized to value 4
